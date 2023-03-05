61aa6ee15504c8ace81ac9b68a763aaa94225c18
Maarten Menten <>
2023-03-05 16:37:09 +0100

    finalized options page

--- src/OptionsPage.php
+++ src/OptionsPage.php
@@ -4,12 +4,25 @@ namespace My\Plugin;
 
 class OptionsPage
 {
+    protected $parent_slug   = '';
+    protected $page_title    = '';
+    protected $menu_title    = '';
+    protected $capability    = '';
+    protected $menu_slug     = '';
+    protected $position      = '';
+    protected $option_name   = '';
+    protected $submit_button = '';
+    protected $page_hook     = '';
+
+    protected $sections = [];
+    protected $fields   = [];
+
     /**
      * Construct
      *
-     * @param $id    string
-     * @param $title string
-     * @param $args  array
+     * @param string $id
+     * @param string $title
+     * @param array  $args
      */
     public function __construct($id, $title, $args = [])
     {
@@ -20,9 +33,9 @@ class OptionsPage
                 'page_title'    => $title,
                 'menu_title'    => $title,
                 'capability'    => 'manage_options',
-                'menu_slug'     => "my_{$id}_options_page",
+                'menu_slug'     => "my_plugin_{$id}_options_page",
                 'position'      => null,
-                'option_name'   => "my_{$id}_options",
+                'option_name'   => "my_plugin_{$id}_options",
                 'submit_button' => __('Update', 'my-plugin'),
             ]
         );
@@ -37,7 +50,86 @@ class OptionsPage
         $this->submit_button = $args['submit_button'];
 
         add_action('admin_init', [$this, 'adminInit']);
-        add_action('admin_menu', [$this, 'adminMenu']);
+        add_action('admin_menu', [$this, 'addMenuPage']);
+        add_action('admin_enqueue_scripts', [$this, 'maybeEnqueueAssets']);
+    }
+
+    /**
+     * Get options
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return get_option($this->option_name, $this->getDefaultOptions());
+    }
+
+    /**
+     * Get option
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function getOption($name)
+    {
+        $options = $this->getOptions();
+
+        if (isset($options[$name])) {
+            return $options[$name];
+        }
+
+        return null;
+    }
+
+    /**
+     * Get default options
+     *
+     * @return array
+     */
+    public function getDefaultOptions()
+    {
+        return [];
+    }
+
+    /**
+     * Get field ID
+     *
+     * @param string $field
+     *
+     * @return string
+     */
+    public function getFieldId($field)
+    {
+        return "{$this->menu_slug}-$field";
+    }
+
+    /**
+     * Get field name
+     *
+     * @param string $field
+     *
+     * @return string
+     */
+    public function getFieldName($field)
+    {
+        return "{$this->option_name}[$field]";
+    }
+
+    /**
+     * Add section
+     */
+    public function addSection($id, $title, $callback, $args = [])
+    {
+        $this->sections[$id] = compact('id', 'title', 'callback', 'args');
+    }
+
+    /**
+     * Add section
+     */
+    public function addField($id, $title, $callback, $section = 'default', $args = [])
+    {
+        $this->fields[$id] = compact('id', 'title', 'callback', 'section', 'args');
     }
 
     /**
@@ -47,46 +139,58 @@ class OptionsPage
      */
     public function adminInit()
     {
-        register_setting($this->menu_slug, $this->option_name, [$this, 'sanitizeInput']);
-    }
+        register_setting($this->menu_slug, $this->option_name, [$this, 'sanitizeOptions']);
 
-    /**
-     * Admin Menu
-     *
-     * @return void
-     */
-    public function adminMenu()
-    {
-        if ($this->parent_slug) {
-            add_submenu_page($this->parent_slug, $this->page_title, $this->menu_title, $this->capability, $this->menu_slug, [$this, 'render'], $this->position);
-        } else {
-            add_menu_page($this->page_title, $this->menu_title, $this->capability, $this->menu_slug, [$this, 'render'], $this->position);
+        foreach ($this->sections as $section) {
+            add_settings_section($section['id'], $section['title'], $section['callback'], $this->menu_slug, $section['args']);
+        }
+
+        foreach ($this->fields as $field) {
+            add_settings_field($field['id'], $field['title'], $field['callback'], $this->menu_slug, $field['section'], $field['args']);
         }
     }
 
     /**
-     * Render
+     * Add menu page
      *
      * @return void
      */
-    public function render()
+    public function addMenuPage()
+    {
+        if ($this->parent_slug) {
+            $this->page_hook = add_submenu_page($this->parent_slug, $this->page_title, $this->menu_title, $this->capability, $this->menu_slug, [$this, 'renderPage'], $this->position);
+        } else {
+            $this->page_hook = add_menu_page($this->page_title, $this->menu_title, $this->capability, $this->menu_slug, [$this, 'renderPage'], $this->position);
+        }
+    }
+
+    /**
+     * Render page
+     *
+     * @return void
+     */
+    public function renderPage()
     {
         ?>
 
         <div class="wrap">
 
-            <h1><?php echo get_admin_page_title(); ?></h1>
+            <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
 
-            <?php settings_fields($this->menu_slug); ?>
-            <?php do_settings_sections($this->menu_slug); ?>
+            <form action="options.php" method="post">
 
-            <?php
+                <?php
 
-            if ($this->submit_button) {
-                submit_button($this->submit_button);
-            }
+                settings_fields($this->menu_slug);
+                do_settings_sections($this->menu_slug);
 
-            ?>
+                if ($this->submit_button) {
+                    echo submit_button($this->submit_button);
+                }
+
+                ?>
+
+            </form>
 
         </div>
 
@@ -94,15 +198,39 @@ class OptionsPage
     }
 
     /**
-     * Sanitize input
+     * Sanitize options
      *
-     * @param $input array
+     * @param array $input
      *
-     * @return void
+     * @return array
      */
-    public function sanitizeInput($input)
+    public function sanitizeOptions($input)
     {
         return $input;
     }
-}
 
+    /**
+     * Maybe Enqueue assets
+     *
+     * @return void
+     */
+    public function maybeEnqueueAssets()
+    {
+        $screen = get_current_screen();
+
+        if ($screen->id != $this->page_hook) {
+            return;
+        }
+
+        $this->enqueueAssets();
+    }
+
+    /**
+     * Enqueue assets
+     *
+     * @return void
+     */
+    public function enqueueAssets()
+    {
+    }
+}
