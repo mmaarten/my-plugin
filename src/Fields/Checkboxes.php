e1c31a9c6362119d277dbf7f712b1907b6800d9a
Maarten Menten <>
2023-03-07 15:52:07 +0100

    Added checkboxes field type.

---
+++ src/Fields/Checkboxes.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace My\Plugin\Fields;
+
+use My\Plugin\Help;
+
+class Checkboxes extends Base
+{
+    /**
+     * Construct
+     */
+    public function __construct()
+    {
+        parent::__construct('checkboxes');
+    }
+
+    /**
+     * Render field
+     *
+     * @param array $field
+     *
+     * @return void
+     */
+    public function renderField($field)
+    {
+        echo '<ul>';
+
+        foreach ($field['choices'] as $option_value => $option_label) {
+            printf(
+                '<li><label><input type="checkbox" name="%1$s[]" value="%2$s"%3$s>%4$s</label></li>',
+                esc_attr($field['name']),
+                esc_attr($option_value),
+                checked(in_array($option_value, $field['value']), true, false),
+                esc_html($option_label)
+            );
+        }
+
+        echo '</ul>';
+    }
+
+    /**
+     * Sanitize field
+     *
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    public function sanitizeFieldValue($value, $field)
+    {
+        return is_array($value) ? $value : [];
+    }
+}
